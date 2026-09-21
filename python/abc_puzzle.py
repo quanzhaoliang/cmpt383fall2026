e7a870e2f6b4399fa48b9fe2821f6c16eea019f1
Sheldon Liang <quanzhao_liang@sfu.ca>
2026-09-21 12:31:58 -0700

    Save Practice Process

--- python/abc_puzzle.py
+++ python/abc_puzzle.py
@@ -14,20 +14,51 @@
 # Solution 1: use for-loops (and no comprehensions)
 def sol_for_loops():
     solutions = []
-    # ...
+    num_solutions = 0
 
-    print(solutions)
+    for a in range(1, 10):
+        for b in range(10):
+            if a == b: continue
+            for c in range(1, 10):
+                if a == c or b == c: 
+                    continue
+                abc = a*100 + b*10 + c
+                cba = c*100 + b*10 + a
+                sum = abc + cba
+                if sum > 1000: continue
+                da = sum % 10
+                db = sum // 10 % 10
+                dc = sum // 100 % 10
+                if abc < cba and da == db and db == dc:
+                    num_solutions += 1
+                    solutions.append([a, b, c])
+
+
+    print(f'Number of solution: {num_solutions}')
+    print(f'Solutions: {solutions}')
 
 # Solution 2: use a single list comprehension (and no loops, no walrus operator)
 def sol_list_comprehension():
-    solutions = []
-    # ...
+    solutions = [(a, b, c) for a in range(1, 10) 
+                            for b in range(10) if a != b
+                            for c in range(1, 10) if a != c or b != c
+                            if (a*100 + b*10 + c) < (c*100 + b*10 + a)
+                            if len(str((a*100 + b*10 + c) + (c*100 + b*10 + a))) == 3 and len(set(str((a*100 + b*10 + c) + (c*100 + b*10 + a)))) == 1]
 
-    print(solutions)
+    print(f'Number of solution: {len(solutions)}')
+    print(f'Solutions: {solutions}')
 
 # Solution 3: use a single list comprehension and the walrus operator
 def sol_walrus():
-    solutions = []
-    # ...
+    solutions = [(a, b, c) for a in range(1, 10) 
+                            for b in range(10) if a != b
+                            for c in range(1, 10) if a != c or b != c
+                            if (abc := a*100 + b*10 + c) < (cba := c*100 + b*10 + a)
+                            if len(str((abc + cba))) == 3 and len(set(str(abc + cba))) == 1]
 
-    print(solutions)
+    print(f'Number of solution: {len(solutions)}')
+    print(f'Solutions: {solutions}')
+
+sol_for_loops()
+sol_list_comprehension()
+sol_walrus()
